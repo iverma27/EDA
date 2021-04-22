2e0d3c4e720ea10a88b64d7de1a33175b87bb02b
iverma27 <iverma@ufl.edu>
2021-04-22 19:54:51 -0400

    added level checking in main, fixed errors with fprintf and error checking, will be removing useless parts of code.

--- redlevels_2.m
+++ redlevels_2.m
@@ -1,7 +1,1 @@
 %function [rpix_in, rpix_out, redarea, sev] = redlevels(picname)
-img = imread('num3.jpg');
-img = img(:, :, 1);
-section = roipoly(img);
-red_vals = img(section);
-red_sum = sum(red_vals, 1);
-red_mean = sum(red_vals, 1) / sum(red_vals~=0, 1);
